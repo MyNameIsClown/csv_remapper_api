b0a2357169e8044f5cd976cf833e8fbe120dcbf9
Victor Carrasco Laptop <vc.business.oo@gmail.com>
2025-07-28 13:56:12 +0200

    feat: Readme updated. Added endpoint to upload files and renaming one key.

---
+++ README.md
@@ -0,0 +1,19 @@
+# csv-remapper-api
+
+**csv-remapper-api** is the api for the librarie [`csv-remapper-lib`](https://github.com/MyNameIsClown/csv-remapper-lib) Python library for remapping and transforming CSV files.
+
+## ✨ Features
+
+- RESTful API built with FastAPI
+- Integrates `csv-remapper-lib` to process CSV files
+- Designed to be modular, testable, and easy to extend
+- Python 3.12+
+- Dependency management with Poetry
+
+## 🚀 Getting Started
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/your-username/csv-remapper-api.git
+cd csv-remapper-api
--- app/main.py
+++ app/main.py
@@ -1,3 +1,37 @@
-from fastapi import FastAPI, File
+import uvicorn
+import uuid
+from fastapi import FastAPI, File, UploadFile
+from csv_remapper_lib import CSVFile
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class RenameKeyModel(BaseModel):
+    file_uuid: str
+    key_names: list[str]
+
+
+@app.post("/create_csv_object/")
+async def create_upload_file(file: UploadFile):
+    contents = await file.read()
+    unique_uid = uuid.uuid4()
+    csv_route = "files/%s" % (str(unique_uid) + ".csv")
+    # Save file
+    with open(csv_route, "x") as f:
+        str_content = contents.decode()
+        f.write(contents.decode().replace("\r\n", "\n"))
+    return {"csv_uuid": str(unique_uid)}
+
+@app.post("/rename_key/")
+def rename_key(key_names: RenameKeyModel):
+    if key_names.file_uuid == "":
+        return {"Error, file not found"}
+    # Found file
+    csv_route = "files/%s" % (str(key_names.file_uuid) + ".csv")
+    csv = CSVFile(csv_route)
+    csv.rename_key(key_names.key_names[0], key_names.key_names[1])
+    csv.save()
+    return {"Rename success"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
